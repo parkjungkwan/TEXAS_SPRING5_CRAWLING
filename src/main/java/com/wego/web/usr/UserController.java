224e6c68ad6865126e9c9af6d6ff4c6c48c7bb06
parkjungkwan <pakjkwan@gmail.com>
2019-11-21 16:14:43 +0900

    16:41

---
+++ src/main/java/com/wego/web/usr/UserController.java
@@ -0,0 +1,32 @@
+package com.wego.web.usr;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import lombok.extern.log4j.Log4j;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+	
+	
+
+}
